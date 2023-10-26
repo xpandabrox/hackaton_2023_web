518eb4ec443f4e6a5abb8f8ebf0ad66bcc9b70a7
tassicadami <148878662+tassicadami@users.noreply.github.com>
2023-10-26 14:38:11 +0300

    Add files via upload

--- avtor/TESTING_SCRIPTS.js
+++ avtor/TESTING_SCRIPTS.js
@@ -1,28 +1,11 @@
 
-// function Imba(){
+function Imba(){
     
     
-    // if((document.getElementById("login").value=="admin")&&(document.getElementById("psw").value=="12345")){
-    //     window.location.assign("http://bimbam/HACATOON_2/sait_admin/test.php");
-    // }else{
-    //     window.location.assign("http://bimbam/HACATOON_2/sait_user/test.php");
-    // }
-    const mysql = require("mysql2");
-  
-    const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  port:"3336",
-  database: "HACATOON",
-  password: ""
-  
-    }   );
- connection.connect(function(err){
-    if (err) {
-      return console.error("Ошибка: " + err.message);
+    if((document.getElementById("login").value=="admin")&&(document.getElementById("psw").value=="12345")){
+        window.location.assign("http://bimbam/HACATOON_2/sait_admin/test.php");
+    }else{
+        window.location.assign("http://bimbam/HACATOON_2/sait_user/test.php");
     }
-    else{
-      console.log("Подключение к серверу MySQL успешно установлено");
-    }
- });
-// }
+    
+}
