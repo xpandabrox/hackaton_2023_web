bde2a7624619b9191daa29eddd1207defd0200da
tassicadami <148878662+tassicadami@users.noreply.github.com>
2023-10-26 20:00:09 +0300

    Add files via upload

--- sait_admin/test.php
+++ sait_admin/test.php
@@ -82,7 +82,7 @@ if($conn->connect_error){
         <td> 
     <div class="work">
         <h1>Выполнено</h1>
-        <div class="t2"><h7>• Task 1</h7>
+        <!-- <div class="t2"><h7>• Task 1</h7>
             <div>Инфа внутри задачи.
                 <div class="date">
                     Установите сроки выполнения задачи
@@ -92,22 +92,77 @@ if($conn->connect_error){
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
+        <?php
+$conn = new mysqli('localhost', 'root', '', 'HACATOON');
+if($conn->connect_error){
+    die('Ошибка: ' . $conn->connect_error);
+}
+    $sql = 'SELECT * FROM TASK';
+    $id_task = 'SELECT id_task FROM TASK';
+    $result = $conn->query($sql);
+    $result_id = $conn->query($id_task);
+    foreach($result  as $row){
+            echo
+            '<div class="t2" >'
+            .'<h7>'
+            .$row['name_task']
+            .'</h7>'
+            .  '<div> '
+            . $row['cel_task'] 
+            .'<div class="date"> Установите сроки выполнения задачи <div> Дата '
+            .$row['data_task']
+            .'   Время '
+            .$row['time_task']
+            .' <div>Количество участников '
+            .$row['numbers_task']
+            .'<div> Приоритет '
+            .$row['priority_task'] 
+            .'</div>'
+            .'</div>'
+            .'</div>'
+            .'</div>'
+            .'</div>'
+            .'</div>';    
+    }
+?>
         
         <td> 
     <div class="denied">
         <h1>Отложено</h1>
-        <div class="t3"><h7>• Task 1</h7>
-            <div>Инфа внутри задачи.
-                <div class="date">
-                    Установите сроки выполнения задачи
-                    <div>
-                        <input type="date">
-                        <input type="time">
-                    </div>
-                </div>
-            </div>
-        </div>
+        <?php
+$conn = new mysqli('localhost', 'root', '', 'HACATOON');
+if($conn->connect_error){
+    die('Ошибка: ' . $conn->connect_error);
+}
+    $sql = 'SELECT * FROM TASK';
+    $id_task = 'SELECT id_task FROM TASK';
+    $result = $conn->query($sql);
+    $result_id = $conn->query($id_task);
+    foreach($result  as $row){
+            echo
+            '<div class="t3" >'
+            .'<h7>'
+            .$row['name_task']
+            .'</h7>'
+            .  '<div> '
+            . $row['cel_task'] 
+            .'<div class="date"> Установите сроки выполнения задачи <div> Дата '
+            .$row['data_task']
+            .'   Время '
+            .$row['time_task']
+            .' <div>Количество участников '
+            .$row['numbers_task']
+            .'<div> Приоритет '
+            .$row['priority_task'] 
+            .'</div>'
+            .'</div>'
+            .'</div>'
+            .'</div>'
+            .'</div>'
+            .'</div>';    
+    }
+?>
 </tr>
 </table>
 <div class="JJJ" id="JJJ">
